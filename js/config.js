7353ad8869c4871c4940580d567fd9913100b91a
adrielgaliano <adriel.galiano.maina@gmail.com>
2026-07-06 11:16:44 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase
 
 // ⚠️ coneccion con base de datos
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: "AIzaSyB-YhYo3ns9Qv4VqLtoaQbFSdVc-ab7n-8",
+    authDomain: "huella-consciente-db.firebaseapp.com",
+    projectId: "huella-consciente-db",
+    storageBucket: "huella-consciente-db.firebasestorage.app",
+    messagingSenderId: "391481979356",
+    appId: "1:391481979356:web:df90462f7e6bc62b5f5f19",
+    measurementId: "G-LQREM7ZJ3J"
   };
 
 const app = initializeApp(firebaseConfig);
